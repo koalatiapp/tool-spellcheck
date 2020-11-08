72df57f980ec3152be0674f2af747a4e32a81c4c
Émile Perron <contact@emileperron.com>
2020-11-08 15:00:15 -0500

    Drastically improved the _extractContent method, adding additional filters, conditions and assumptions.

--- tool.js
+++ tool.js
@@ -15,7 +15,7 @@ class Tool {
             throw new Error("The language of the content could not be identified.");
         }
 
-
+        console.log(this._contentText);
     }
 
     get results() {
@@ -40,38 +40,76 @@ class Tool {
 
     async _extractContent() {
         return await this.page.evaluate(() => {
-            // we'll be doing some manipulation on the nodes, so we keep it non-destructive
             const pageNode = document.body.cloneNode(true);
+            const originalNodes = document.body.querySelectorAll('*');
+            const clonedNodes = pageNode.querySelectorAll('*');
 
-            // get rid of unwanted elements for copy text
+            // Apply computed styles as inline CSS for every node, as window.getComputedStyle isn't available outside the DOM
+            for (let i = 0; i < originalNodes.length; i++) {
+                clonedNodes[i].setAttribute('style', window.getComputedStyle(originalNodes[i]).cssText);
+            }
+
+            // Get rid of unwanted elements for copy text
             for (const unwantedNode of pageNode.querySelectorAll('script, style, noscript, code')) {
                 unwantedNode.remove();
             }
 
-            // prevent <br> from causing stuck-together words
+            // Prevent <br> from causing stuck-together words
             for (const brNode of pageNode.querySelectorAll('br')) {
-                brNode.outerHTML = " ";
+                brNode.outerHTML = brNode.nextSibling && brNode.nextSibling.nodeValue && brNode.nextSibling.nodeValue.trim().length ? ' ' : '\n';
             }
 
-            // flex childs are rarely words forming a sentence: break them apart
-            for (const node of pageNode.querySelectorAll('*')) {
-                if (window.getComputedStyle(node).display.toLowerCase().indexOf('flex') != -1) {
-                    for (const child of node.children) {
-                        child.outerHTML = "\n\n" + child.outerHTML;
+            // Replace images with their alt text if they have one
+            for (const imgNode of pageNode.querySelectorAll('img[alt]:not([alt=""])')) {
+                imgNode.outerHTML = '\n' + imgNode.alt + '\n';
+            }
+
+            // Flex, block or grid display links with that only contain text can most likely be on their own line
+            for (const linkNode of pageNode.querySelectorAll('a')) {
+                const display = linkNode.style.display.toLowerCase();
+                if (display == 'block' || display.indexOf('flex') != -1 || display.indexOf('grid') != -1) {
+                    if (![...linkNode.childNodes].filter((node) => { return node.nodeName != '#text'; }).length) {
+                        linkNode.innerHTML = "\n\n" + linkNode.innerHTML;
                     }
                 }
             }
 
-            // simple fix for minified HTML
+            // Flex childs are rarely words forming a sentence: break them apart
+            for (const node of pageNode.querySelectorAll('*')) {
+                if (node.style.display.toLowerCase().indexOf('flex') != -1) {
+                    for (const child of node.children) {
+                        child.innerHTML = "\n\n" + child.innerHTML;
+                    }
+                }
+            }
+
+            // Simple fix for minified HTML
             pageNode.innerHTML = pageNode.innerHTML.replace(/></g, '> <');
 
-            // home stretch...
-            const content = (pageNode.innerText || '')
+            // Make sure headings are on their own lines - they should be "self-sufficient"
+            pageNode.innerHTML = pageNode.innerHTML.replace(/<h([1-6])(.+?)<\/h\1>/g, '\n<h$1$2</h1>\n');
+
+            // Home stretch...
+            const rawContent = (pageNode.innerText || '')
                 .replace(/(\s{2,})([A-Z0-9])/g, '$1\n$2') // split blocks that seem to contain multiple sentences or standalone blocks
                 .replace(/\s{3,}/g, '\n') // break everything into single line blocks
-                .replace(/\n.{1,3}\n/g, '\n'); // remove tiny words or tokens that are on their own
+                .replace(/\n.{1,3}\n/g, '\n') // remove tiny words or tokens that are on their own
+                .replace(/ {2,}/g, ' ') // replace multiple spaces by a single one
+                .replace(/^\s(.+)$/gm, '$1'); // remove spaces at the beginning of lines
 
-            return content;
+            // Get an array of strings without duplicates via the Set constructor and spread operator
+            let contentStrings = [...new Set(rawContent.split('\n'))];
+
+            // Filter out strings that aren't really adequate for spellchecking
+            contentStrings = contentStrings.filter((string) => {
+                if (string.length < 3) { return false; }
+                if (string.replace(/[0-9.,%$:]/g, '').length < 3) { return false; }
+                if (!/[a-zA-Z]{2,}/.test(string)) { return false; }
+
+                return true;
+            });
+
+            return contentStrings.join('\n');
         });
     }
 
@@ -81,13 +119,18 @@ class Tool {
 
         let detectedLanguages = langDetector.detect(this._contentText);
 
-        for (const result of detectLanguages) {
+        for (const result of detectedLanguages) {
             if (result[0] && result[0] != 'la') {
                 return result[0];
             }
         }
 
         return null;
+    }
+
+    async _processText() {
+        const apiUrl = process.env.LANGUAGETOOL_API_URL || 'https://languagetool.org/api/v2/check';
+        const apiPort = process.env.LANGUAGETOOL_API_PORT || 80;
     }
 }
 
